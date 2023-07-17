ce6ebbd24d8d426d7e6390dbcd06271a0877ea3a
József Bagota <jozsef.bagota@gmail.com>
2023-07-17 13:49:17 +0200

    setuping state and mutations in store and fetching data function in App.vue

--- restaurants-list-app/src/App.vue
+++ restaurants-list-app/src/App.vue
@@ -6,6 +6,25 @@
   <router-view/>
 </template>
 
+<script>
+  export default {
+    components: {
+
+    },
+    async mounted() {
+          try {
+              if(this.$store.state.restaurants.length > 0) {
+                  this.$store.commit('setRestaurantEmpty');
+              }
+              const response = await fetch('https://lajolla.dev-famished.com/api/restaurants');
+              this.$store.state.restaurants = await response.json();
+          } catch (error) {
+              console.error(error);
+          }
+      }
+  }
+</script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- restaurants-list-app/src/store/index.js
+++ restaurants-list-app/src/store/index.js
@@ -1,14 +1,18 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        restaurants: [],
+    },
+    getters: {
+    },
+    mutations: {
+        setRestaurantEmpty(state) {
+            state.restaurants = [];
+        },
+    },
+    actions: {
+    },
+    modules: {
+    }
 })
